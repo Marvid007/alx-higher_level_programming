d718790f7c1e6ac0d18c072488a52f11c7dffde0
Onwuchekwe David <marcusdhavid20@gmail.com>
2023-03-07 17:55:49 -0800

    0x01. Python - if/else, loops, functions updated

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -12,7 +12,7 @@ def fizzbuzz():
     for number in range(1, 101):
 
         if number % 3 == 0 and number % 5 == 0:
-            print("FizzBuzz", end="")
+            print("Fizz Buzz", end="")
         elif number % 3 == 0:
             print("Fizz", end="")
         elif number % 5 == 0:
