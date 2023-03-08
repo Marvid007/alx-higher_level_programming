b857e59da257c671a207ff4b4e9dd86e4a60cad7
Onwuchekwe David <marcusdhavid20@gmail.com>
2023-03-07 17:36:56 -0800

    0x01. Python - if/else, loops, functions updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,6 +4,6 @@
     """Print the alphabeth in reverse order alternating upper- and lower-case.
 """
     i = 0
-    for c in range(ord('Z'), ord('a') - 1, -1):
+    for c in range(ord('z'), ord('a') - 1, -1):
         print("{}".format(chr(c - i)), end="")
         i = 32 if i == 0 else 0
