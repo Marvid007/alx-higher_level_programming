208b4be638548136f9126e04b38014449df97f91
Onwuchekwe David <marcusdhavid20@gmail.com>
2023-06-13 10:18:27 -0700

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
-if (process.argv.length > 3) {
-	  const array = process.argv.slice(2).map(Number);
-
-	  array.splice(array.indexOf(Math.max.apply(null, array)), 1);
-	  console.log(Math.max.apply(null, array));
-} else {
+if (process.argv.length <= 3) {
 	  console.log(0);
+} else {
+	  const args = process.argv.map(Number)
+	    .slice(2, process.argv.length)
+	    .sort((a, b) => a - b);
+	  console.log(args[args.length - 2]);
 }
